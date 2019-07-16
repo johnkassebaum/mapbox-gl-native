201f274256a778380766c2ca182f22b61122cdcd
Alexander Shalamov <alexander.shalamov@mapbox.com>
2019-07-16 17:02:18 +0300

    [core] Update shaping.test.cpp

--- test/text/shaping.test.cpp
+++ test/text/shaping.test.cpp
@@ -36,7 +36,8 @@ TEST(Shaping, ZWSP) {
                           {0.0f, 0.0f}, // translate
                           WritingModeType::Horizontal,
                           bidi,
-                          glyphs);
+                          glyphs,
+                          nullopt);
     };
 
     // 3 lines
